6bed99def9b777e427205f2198efc786edaa984d
James Anderson <me@jamesaanderson.com>
2014-04-26 19:46:59 -0700

    Move coveralls to top of test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,10 @@
+require 'coveralls'
+Coveralls.wear!
+
 require 'delivery'
 require 'minitest/pride'
 require 'minitest/autorun'
 require 'webmock/minitest'
-
-require 'coveralls'
-Coveralls.wear!
 
 def fixture_path
   File.expand_path('../fixtures', __FILE__)
